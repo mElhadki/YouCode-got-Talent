2331084a2aad3bed4de216dcffcadee5e1337604
Ayoub Moustahfid <moustahfidayoub12@gmail.com>
2020-12-17 16:09:45 +0100

    add folder complet with fichier jar

--- src/com/company/Controller/AdminController.java
+++ src/com/company/Controller/AdminController.java
@@ -10,20 +10,30 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.mail.*;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 public class AdminController {
-    Config config;
+	
+
+
+  Config config;
    Scanner scanner = new Scanner(System.in);
     public AdminController() throws SQLException {
         config = new Config("jdbc:mysql://localhost/yc-talents", "root", "");
     }
-
-
+    
     public ArrayList<User> findAllUsers() throws SQLException {
         ArrayList<User> userArrayList = new ArrayList<>();
 
-        String query = "SELECT * FROM admin";
+        String query = "SELECT * FROM user";
         PreparedStatement preparedStatement = config.connection().prepareStatement(query);
         ResultSet resultSet = preparedStatement.executeQuery();
 
@@ -43,11 +53,20 @@ public class AdminController {
         String query = "Update adminsession SET is_connected = true WHERE id_admin=15970010";
         PreparedStatement statement = config.connection().prepareStatement(query);
         statement.executeUpdate();
+        statement.close();
         System.out.println("login admin is success");
     }
 
+    public void adminLoggedOut() throws SQLException {
+    String query = "UPDATE adminsession SET is_connected= FALSE WHERE id = 15970010";
+    PreparedStatement statement = config.connection().prepareStatement(query);
+    statement.executeUpdate();
+    statement.close();
+    System.out.println("Admin is logged out !!!!");
+    }
+
     public List<Participant> findParticipant() throws SQLException {
-        String query = "SELECT * FROM participant";
+        String query = "SELECT * FROM participation";
         Statement statement = config.connection().createStatement();
         ResultSet resultSet = statement.executeQuery(query);
 
@@ -67,12 +86,13 @@ public class AdminController {
         Participant participant = new Participant();
 
         try {
-            String query = "SELECT prt.id_user FROM participant prt user us WHERE prt.id_user = us.id_user AND us.email = '" + email + "'";
+            String query = "SELECT prt.id_user FROM participation prt user us WHERE prt.id_user = us.id_user AND us.email = '" + email + "'";
             PreparedStatement prestmt = config.connection().prepareStatement(query);
             ResultSet resultSet = prestmt.executeQuery(query);
 
             while (resultSet.next()) {
                 System.out.println(resultSet.getString("id_user"));
+                System.out.println(resultSet.getString("description"));
             }
             
            
@@ -83,17 +103,70 @@ public class AdminController {
         return null;
 
     }
+
+ 
+
+   public static void acceptedParticipation(String recepient) throws MessagingException{
+   
+    	Properties properties = new Properties();
+
+       properties.put("mail.smtp.auth", "true");
+       properties.put("mail.smtp.starttls.enable", "true");
+       properties.put("mail.smtp.auth", "true");
+       properties.put("mail.smtp.host", "smtp.gmail.com");
+       properties.put("mail.smtp.port", "587");
+
+    	
+    	String myAccountEmail = "ayoubmousa30@gmail.com";
+    	String password = "ayoubgermany@@--@@";
+    	
+    	Session session = Session.getInstance(properties, new Authenticator() {
+    		@Override
+    		protected PasswordAuthentication getPasswordAuthentication() {
+    			return new PasswordAuthentication(myAccountEmail, password);
+    		}
+    	});
+    	
+    	
+		Message message = prepareMessage(session, myAccountEmail, recepient);
+    	
+    	Transport.send(message);
+    	System.out.println("Message sent successfully");
+
+    }
     
-    public Participant validateParicipant(int UserId) {
+    
+ 
+    private static Message prepareMessage(Session session, String myAccountEmail, String recepient) {
+		// TODO Auto-generated method stub
+    	
+    	try {
+    		Message message = new MimeMessage(session);
+    		message.setFrom(new InternetAddress(myAccountEmail));
+    		message.setRecipient(Message.RecipientType.TO, new InternetAddress(recepient));
+    		message.setSubject("My First Email from Java App");
+    		message.setText("Hey there, \n Look my email !");
+    		return message;
+    	}catch(Exception ex) {
+    		Logger.getLogger(AdminController.class.getName()).log(Level.SEVERE, null, ex);
+    	}
+    	
+    	return null;
+	}
+
+
+   /* public Participant validateParicipant(int UserId) {
     	
         System.out.println("Enter id paricipation :");
         long user_id = scanner.nextLong();
         
+
     	try {
     	String query = "UPDATE participation SET is_accepted=TRUE WHERE user_id =?";
     	PreparedStatement statement = config.connection().prepareStatement(query);
     	statement.setLong(1, UserId);
     	 System.out.println("this participation is accepted");
+    	
     	statement.execute();
     	statement.close();
     	
@@ -105,4 +178,44 @@ public class AdminController {
  
     	return null;
     }
+	*/
+    
+
+    public void validateParticipation(long id_user, long id_category) throws SQLException, MessagingException {
+
+        // Check if id exist in table
+
+        String query = "select * from participation  WHERE id_user = '" + id_user + "' AND id_category = '" + id_category
+                + "'AND is_accepted = false ";
+        PreparedStatement statement = config.connection().prepareStatement(query);
+        ResultSet resultSet = statement.executeQuery(query);
+        if (resultSet.isBeforeFirst()) {
+
+            String query2 = "UPDATE participation SET is_accepted = true WHERE id_user ='" + id_user + "'";
+            PreparedStatement statement2 = config.connection().prepareStatement(query2);
+            statement2.executeUpdate();
+            System.out.println("User Accepted \n");
+
+            // Get the user Email :D
+
+            String query3 = "select * from user where id = '" + id_user + "' ";
+
+            // Get user id from user table using the email
+
+            PreparedStatement statement3 = config.connection().prepareStatement(query3);
+            ResultSet resultSet3 = statement3.executeQuery(query3);
+            String email = "";
+            while (resultSet3.next()) {
+                email = resultSet3.getString("email");
+
+            }
+
+            // Send mail to user
+
+           
+            acceptedParticipation(email);
+        }
+
+    }
+	 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,93 +1,189 @@
 package com.company;
 
+import com.company.Controller.AdminController;
+import com.company.Controller.UserController;
+import com.company.Controller.ParticipantController;
+
+import javax.mail.MessagingException;
 import java.sql.SQLException;
 import java.util.Scanner;
 
-import com.company.Controller.UserController;
-import com.company.models.User;
-
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-	// write your code here
 
-     UserController userController = new UserController();
-     Scanner scanner = new Scanner(System.in);
-      
-     boolean var = true;
-      
-      while(var) {
-    	  /* Depart de l'application
-    	   * ------------ Menu Principal --------------
-    	   * --->> la gestion User
-    	   * --->> la gestion Admin
-    	   * --->> la gestion Participant
-    	   * */
-    	  
-    	 System.out.println("========== Menu de les gestion programmes ============");
-    	 System.out.println("(1) Gestion User");
-    	 System.out.println("(2) Connect Admin");
-    	  
-    	  
-    	  int choix = scanner.nextInt();
-    	  
-    	  switch(choix) {
-    	  case 1:
-    		  boolean variable1 = true;
-    		  while(variable1) {
-    			  System.out.println("(1) Add User");
-    			  System.out.println("(2) find User by Id");
-    			  System.out.println("(3) update User");
-    			  System.out.println("(4) create participation the user");
-    			  System.out.println("(5) End");
-    			  System.out.println("(6) Choix invalid");
-    			  
-    			  
-    			  System.out.println("Choix : ");
-    			  
-    			  int choixUser = scanner.nextInt();
-    			  
-    			  switch(choixUser) {
-    			  case 1:
-    				 
-    				  
-    				  break;
+    public static void main(String[] args) throws SQLException, MessagingException {
+        UserController userController = new UserController();
+        AdminController adminController = new AdminController();
+        ParticipantController participantController = new ParticipantController();
+        
+        
+        Scanner scanner = new Scanner(System.in);
 
-    		      case 2:
-    				  System.out.println("End");
-    				  variable1 = false;
-    				  var = true;
-    				  break;
-    			   default:
-    					  System.out.println("Choix Invalid !!!!");
-    					  break;
+        boolean var = true;
 
-    			  }
-    		  }
-    		  break;
-    		  
-    	  case 2:
-    		  boolean variable2 = true;
-    		  while(variable2) {
-    			  System.out.println("(1) Connect Admin");
-    			  System.out.println("(2) find All User");
-    			  System.out.println("(3) find participation");
-    			  System.out.println("(4) find particiapation by user email");
-    			  System.out.println("(5) validate particiapation");
-    			  
-    			  
-    			  System.out.println("Choix :");
-    			  
-    			  int choixAdmin = scanner.nextInt();
-    			  
-    			  switch(choixAdmin) {
-    			  case 1:
-    				  break;
-    				  
-    				  
-    			  }
-    		  }
-    	  }
-      }
+        while(var) {
+            /* Depart de l'application
+             * ------------ Menu Principal --------------
+             * --->> la gestion User
+             * --->> la gestion Admin
+             * --->> la gestion Participant
+             * */
+
+            System.out.println("========== Menu de les gestion programmes ============");
+            System.out.println("(1) Gestion User");
+            System.out.println("(2) Connect Admin");
+
+
+            int choix = scanner.nextInt();
+
+            switch(choix) {
+                case 1:
+                    boolean variable1 = true;
+                    while(variable1) {
+                        System.out.println("(1) Add User");
+                        System.out.println("(2) find User by Id");
+                        System.out.println("(3) update User");
+                        System.out.println("(4) create participation the user");
+                        System.out.println("(5) End");
+                        System.out.println("(6) Choix invalid");
+
+
+                        System.out.println("Choix : ");
+
+                        int choixUser = scanner.nextInt();
+
+                        switch(choixUser) {
+                        case 1:
+                       	 
+                       	 userController.add();
+                                  
+                            break;
+                            
+                        case 2: 
+                       	 
+                       	 userController.searchById();
+                       	 break;
+                       	 
+                        case 3: 
+                       	 
+                       	 userController.update();
+                       	 break;
+                       	 
+                        case 4:
+                       	 
+                        	participantController.addParticipant();
+                       	 break;
+                            case 5:
+                                System.out.println("End");
+                                variable1 = false;
+                                var = true;
+                                break;
+                            default:
+                                System.out.println("Choix Invalid !!!!");
+                                break;
+
+                        }
+                    }
+                    break;
+
+                case 2:
+                    boolean variable2 = true;
+                    System.out.println(" !!!!!!   Obliger Connecter Admin !!!!!!!!!!!");
+                    System.out.println("+++++++++++ Log in +++++++++++");
+                    
+                    System.out.println("Entrer votre Email :");
+                    String email = scanner.next();
+                    
+                    System.out.println("Entrer votre password :");
+                    String password = scanner.next();
+                    
+
+                    // Check If The Admin is Logged
+                    if(email.equals("ayoubmousa30@gmail.com") && password.equals("ayoubgermany@@--@@")) {
+                    	try {
+                    		adminController.adminConnect();
+                    	}catch(SQLException ex) {
+                    		ex.printStackTrace();
+                    	}
+                    	
+                    	 while(variable2) {
+                             System.out.println("(1) find All User");
+                             System.out.println("(2) find participation");
+                             System.out.println("(3) find particiapation by user email");
+                             System.out.println("(4) validate particiapation");
+                             System.out.println("(5) Loug out Admin");
+                             System.out.println("(6) End");
+                             System.out.println("(7) Choix invalid");
+
+
+                             System.out.println("Choix : ");
+                             int choixAdmin = scanner.nextInt();
+
+                             switch(choixAdmin){
+                                 case 1:
+                                	 try {
+                                		 adminController.findAllUsers();
+                                	 }catch(SQLException ex) {
+                                		 ex.printStackTrace();
+                                	 }
+                                     break;
+                                     
+                                
+                                	 
+                                 case 2:
+                                	try {
+                                		 adminController.findParticipant();
+                                	}catch(SQLException ex2) {
+                                      ex2.printStackTrace();                                		
+                                	}
+                                	 break;
+                                	 
+                                 case 3:
+								     adminController.findParticipantByUserEmail();
+                                	 break;
+                                 case 4:
+                                     try {
+                                             System.out.println("Please enter User Id \n");
+                                             long userId = scanner.nextLong();
+                                             System.out.println("Please enter Category Id \n");
+                                             long idCategory = scanner.nextLong();
+
+                                             adminController.validateParticipation(userId, idCategory);
+
+                                       
+                                     } catch (SQLException e) {
+                                         e.printStackTrace();
+                                     }
+                                     break;
+                                 case 5 :
+                                	 adminController.adminLoggedOut();
+                                	 break;
+                                 case 6:
+                                 	  System.out.println("End");
+                                       variable1 = false;
+                                       var = true;
+                                       break;
+
+                                 default:
+                                 	System.out.println("Choix Invalid !!!!");
+                                     break;
+                             }
+                         }
+                    }
+                    break;
+
+                case 3 :
+                    System.out.println("========== END ============");
+                    System.out.println(0);
+                    break;
+
+                default:
+                    System.out.println("Choix invalid !!!!!!!!!");
+                    break;
+            }
+        }
+
     }
+    
+
 }
